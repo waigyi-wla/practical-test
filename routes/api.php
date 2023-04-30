48f9812808c744d2f6fdced5e36083709d10af5e
Wai Lwin Aung <you@example.com>
2023-04-30 10:04:51 +0630

    user register and login with sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\V1\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::prefix('v1')->group(function () {
+    Route::post('login', [AuthController::class, 'signin']);
+    Route::post('register', [AuthController::class, 'signup']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
